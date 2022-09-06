9605b691f46d69578b078bacae839e854e84eb76
themak01 <kmaftab603@gmail.com>
2022-09-06 22:44:08 +0530

    Alert Dialogue app

---
+++ app/src/main/java/com/example/alertdialogue/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.alertdialogue;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements dialogue.ExampleDialogListener {
+    private TextView textViewUsername;
+    private TextView textViewPassword;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewUsername = (TextView) findViewById(R.id.textview_username);
+        textViewPassword = (TextView) findViewById(R.id.textview_password);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+
+    public void openDialog() {
+        dialogue exampleDialog = new dialogue();
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
+    }
+
+    @Override
+    public void applyTexts(String username, String password) {
+        textViewUsername.setText(username);
+        textViewPassword.setText(password);
+    }
+}
